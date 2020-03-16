5ff8678d79ccf2425ccaaa02bf4d5e871147ecb5
seungrokoh <devdavidoh@gmail.com>
2020-03-16 13:16:00 +0900

    refactoring : Sort 함수 반환값 제거
    * sort.Sort() 함수와 동일하게 return 값 제거

--- practice_problem/chapter3/problem02/sort.go
+++ practice_problem/chapter3/problem02/sort.go
@@ -1,6 +1,6 @@
 package problem02
 
-func SelectionSort(nums []int) []int {
+func SelectionSort(nums []int) {
 	for i := 0; i < len(nums) - 1; i++ {
 		min := i
 		for j := i + 1; j < len(nums); j++ {
@@ -10,5 +10,4 @@ func SelectionSort(nums []int) []int {
 		}
 		nums[i], nums[min] = nums[min], nums[i]
 	}
-	return nums
 }
--- practice_problem/chapter3/problem02/sort_test.go
+++ practice_problem/chapter3/problem02/sort_test.go
@@ -9,7 +9,8 @@ var nums = []int{3, 1, 4, 2 ,5, 9, 8 ,7, 6}
 var sortedNums = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 func TestSelectionSort(t *testing.T) {
-	got := SelectionSort(nums)
+	SelectionSort(nums)
+	got := nums
 	want := sortedNums
 
 	if !reflect.DeepEqual(got, want) {
