bcc4ca99f0e0665b69c40f1b27ede6397fe5775b
seungrokoh <devdavidoh@gmail.com>
2020-03-16 13:14:15 +0900

    refactoring : 한글인지 아닌지 확인하는 코드 추가

--- practice_problem/chapter3/problem01/hangul.go
+++ practice_problem/chapter3/problem01/hangul.go
@@ -1,35 +1,49 @@
 package problem01
 
+import "errors"
+
 var (
 	un = "은 맛있다"
 	nun = "는 맛있다"
+
+	ErrNotHangul = errors.New("it is not hangul")
+
+	hangulStart = rune(44032)
+	hangulEnd = rune(55204)
+	numEnds = 28
 )
 
 func PrintFruitDelicious(fruits []string) []string {
-	result := make([]string, len(fruits))
+	index := 0
+	var result []string
 
-	for i, fruit := range fruits {
-		if HasConsonantSuffix(fruit) {
-			// 받침이 있는 경우
-			result[i] = fruit + un
-		} else {
-			// 받침이 없는 경우
-			result[i] = fruit + nun
+	for _, fruit := range fruits {
+		// 한글인지 아닌지 error 체크
+		if hasConsonantSuffix, err := HasConsonantSuffix(fruit); err == nil {
+			if hasConsonantSuffix {
+				// 받침이 있는 경우
+				result = append(result, fruit + un)
+			} else {
+				// 받침이 없는 경우
+				result = append(result, fruit + nun)
+			}
+			index++
 		}
 	}
 	return result
 }
 
-var (
-	start = rune(44032)
-	end = rune(55204)
-)
-
 // 한글의 받침이 있는지 없는지 확인하는 함수
-func HasConsonantSuffix(s string) bool {
-	numEnds := 28
+// 한글이 아닐 경우 ErrNotHangul을 리턴합니다.
+func HasConsonantSuffix(s string) (bool, error) {
 	hangul := []rune(s)
+	lastConsonantSuffix := hangul[len(hangul) - 1]
+
+	if hangulStart > lastConsonantSuffix || lastConsonantSuffix >= hangulEnd {
+		// 한글이 아닐 경우
+		return false, ErrNotHangul
+	}
 	// 마지막 글자만을 확인
-	index := int(hangul[len(hangul) - 1] - start)
-	return index % numEnds != 0
+	index := int(hangul[len(hangul) - 1] - hangulStart)
+	return index % numEnds != 0, nil
 }
--- practice_problem/chapter3/problem01/hangul_test.go
+++ practice_problem/chapter3/problem01/hangul_test.go
@@ -6,11 +6,28 @@ import (
 )
 
 func TestPrintFruitDelicious(t *testing.T) {
-	fruits := []string{"사과", "바나나", "토마토", "귤"}
 
-	got := PrintFruitDelicious(fruits)
-	want := []string{"사과는 맛있다", "바나나는 맛있다", "토마토는 맛있다", "귤은 맛있다"}
+	t.Run("only hangul", func(t *testing.T) {
+		fruits := []string{"사과", "바나나", "토마토", "귤"}
 
+		got := PrintFruitDelicious(fruits)
+		want := []string{"사과는 맛있다", "바나나는 맛있다", "토마토는 맛있다", "귤은 맛있다"}
+
+		assertError(t, got, want)
+	})
+
+	t.Run("contain not hangul", func(t *testing.T) {
+		fruits := []string{"사과", "바나나", "tomato", "귤"}
+
+		got := PrintFruitDelicious(fruits)
+		want := []string{"사과는 맛있다", "바나나는 맛있다", "귤은 맛있다"}
+
+		assertError(t, got, want)
+	})
+}
+
+func assertError(t *testing.T, got, want []string) {
+	t.Helper()
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v, want %v", got, want)
 	}
