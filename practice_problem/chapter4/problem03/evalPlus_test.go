86ad92c3054ab00a55b56c5b9b78f940bee97f6d
seungrokoh <devdavidoh@gmail.com>
2020-03-18 02:01:10 +0900

    add : 테스트 케이스 추가

--- practice_problem/chapter4/problem03/evalPlus_test.go
+++ practice_problem/chapter4/problem03/evalPlus_test.go
@@ -38,7 +38,9 @@ func TestEval(t *testing.T) {
 
 	in := strings.Join([]string{"다들 그 동안 고생이 많았다.",
 		"첫째는 분당에 있는 { 2 ** 4 * 3 }평 아파트를 갖거라.",
-		"둘째는 임야 { 10 ** 5 mod 7777 }평을 가져라."}, "\n")
+		"둘째는 임야 { 10 ** 5 mod 7777 }평을 가져라.",
+		"막내는 { 10000 - ( 10 ** 5 mod 7777 ) }평 임야를 갖고",
+		"배기량 { 711 * 8 / 9 }cc의 경운기 및 { 711 * 8 / 9 * 3}cc의 승용차를 갖거라."}, "\n")
 
 	rx := regexp.MustCompile(`{[^}]+}`)
 	got := rx.ReplaceAllStringFunc(in, func(expr string) string {
@@ -50,7 +52,9 @@ func TestEval(t *testing.T) {
 
 	want := strings.Join([]string{"다들 그 동안 고생이 많았다.",
 		"첫째는 분당에 있는 48평 아파트를 갖거라.",
-		"둘째는 임야 6676평을 가져라."}, "\n")
+		"둘째는 임야 6676평을 가져라.",
+		"막내는 3324평 임야를 갖고",
+		"배기량 632cc의 경운기 및 1896cc의 승용차를 갖거라."}, "\n")
 
 	if got != want {
 		t.Errorf("got %s, want %s", got, want)
