93c7733d0c3f6c2a7eadc35f9f7f391d5de997ab
sjnam <jsunam@gmail.com>
2020-03-19 00:18:01 +0900

    변수 v도 for 안으로

--- summary/chapter4/fibonacci_with_closure/fibonacci_test.go
+++ summary/chapter4/fibonacci_with_closure/fibonacci_test.go
@@ -3,9 +3,8 @@ package practice
 import "testing"
 
 func TestFibonacci(t *testing.T) {
-	v := 0
 	want := []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55}
-	for i, fib := 0, Fibonacci(); i < len(want); i, v = i+1, fib() {
+	for i, v, fib := 0, 0, Fibonacci(); i < len(want); i, v = i+1, fib() {
 		if v != want[i] {
 			t.Errorf("got %v, want %v", v, want[i])
 		}
