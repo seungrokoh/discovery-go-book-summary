e2f8815b5b3f89dc687494c36b46d2adc91a6b7a
sjnam <jsunam@gmail.com>
2020-03-18 18:03:15 +0900

    len() 이용

--- summary/chapter4/fibonacci_with_closure/fibonacci_test.go
+++ summary/chapter4/fibonacci_with_closure/fibonacci_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestFibonacci(t *testing.T) {
 	v := 0
 	want := []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55}
-	for i, fib := 0, Fibonacci(); i < 10; i, v = i+1, fib() {
+	for i, fib := 0, Fibonacci(); i < len(want); i, v = i+1, fib() {
 		if v != want[i] {
 			t.Errorf("got %v, want %v", v, want[i])
 		}
