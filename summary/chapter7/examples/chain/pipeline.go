2c684f45ceb73321de2778aa501d73dabb77cb31
seungrokoh <devdavidoh@gmail.com>
2020-03-24 11:51:50 +0900

    add : FanIn 테스트 추가

--- summary/chapter7/examples/chain/pipeline.go
+++ summary/chapter7/examples/chain/pipeline.go
@@ -33,3 +33,24 @@ func Chain(ps ...IntPipe) IntPipe {
 		return c
 	}
 }
+
+func FanIn(ins ...<-chan int) <-chan int {
+	out := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(len(ins))
+
+	for _, in := range ins {
+		go func(in <-chan int) {
+			defer wg.Done()
+			for num := range in {
+				out <- num
+			}
+		}(in)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
--- summary/chapter7/examples/chain/pipeline_test.go
+++ summary/chapter7/examples/chain/pipeline_test.go
@@ -25,3 +25,31 @@ func TestChain(t *testing.T) {
 		t.Errorf("got %v, want %v", got, want)
 	}
 }
+
+func TestFanIn(t *testing.T) {
+	c1 := make(chan int)
+	c2 := make(chan int)
+	c3 := make(chan int)
+
+	sendInts := func(c chan<- int, begin, end int) {
+		defer close(c)
+		for i := begin; i < end; i++ {
+			c <- i
+		}
+	}
+	go sendInts(c1, 11, 14)
+	go sendInts(c2, 21, 23)
+	go sendInts(c3, 31, 35)
+
+	var got []int
+	var want = []int{11, 12, 13, 21, 22, 31, 32, 33, 34}
+
+	for i := range FanIn(c1, c2, c3) {
+		got = append(got, i)
+	}
+	sort.Ints(got)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
